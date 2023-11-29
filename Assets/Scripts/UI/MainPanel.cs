aa9d173c670a47d8576efb102acd3b2ce05dc11d
DESKTOP-6FGN5NP\Egoist <13260963605wjx@gmail.com>
2023-11-29 14:11:15 +0800

    开始界面UI创建及脚本编写

---
+++ Assets/Scripts/UI/MainPanel.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+    private Button btn_Start;
+    private Button btn_Shop;
+    private Button btn_Rank;
+    private Button btn_Sound;
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        btn_Start = transform.Find("btn_Start").GetComponent<Button>();
+        btn_Start.onClick.AddListener(OnStartButtonClick);
+        btn_Shop = transform.Find("btn_Shop").GetComponent<Button>();
+        btn_Shop.onClick.AddListener(OnShopButtonClick);
+        btn_Rank = transform.Find("btn_Rank").GetComponent<Button>();
+        btn_Rank.onClick.AddListener(OnRankButtonClick);
+        btn_Sound = transform.Find("btn_Sound").GetComponent<Button>();
+        btn_Sound.onClick.AddListener(OnSoundButtonClick);
+    }
+
+    /// <summary>
+    /// 开始按钮点击后调用此方法
+    /// </summary>
+    private void OnStartButtonClick()
+    {
+
+    }
+
+    /// <summary>
+    /// 商店按钮点击后调用此方法
+    /// </summary>
+    private void OnShopButtonClick()
+    {
+
+    }
+
+    /// <summary>
+    /// Rank按钮点击后调用此方法
+    /// </summary>
+    private void OnRankButtonClick()
+    {
+
+    }
+
+    /// <summary>
+    /// 声音按钮点击后调用此方法
+    /// </summary>
+    private void OnSoundButtonClick()
+    {
+
+    }
+}
