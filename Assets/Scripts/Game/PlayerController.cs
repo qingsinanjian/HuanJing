f50eab2c3eee0017151a1f6f6651bb2a07605ef8
DESKTOP-6FGN5NP\Egoist <13260963605wjx@gmail.com>
2023-11-30 16:30:58 +0800

    第一种游戏结束的判断

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -5,6 +5,8 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform rayDown;
+    public LayerMask platformLayer;
     /// <summary>
     /// 是否向左移动
     /// </summary>
@@ -15,10 +17,14 @@ public class PlayerController : MonoBehaviour
     private bool isJumping = false;
     private Vector3 nextPlatformLeft, nextPlatformRight;
     private ManagerVars vars;
+    private Rigidbody2D my_Body;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         vars = ManagerVars.GetManagerVars();
+        my_Body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -39,7 +45,25 @@ public class PlayerController : MonoBehaviour
                 }
                 Jump();
             }
+            //游戏结束
+            if(my_Body.velocity.y < 0 && !IsRayPlatform() && !GameManager.Instance.IsGameOver)
+            {
+                spriteRenderer.sortingLayerName = "Default";
+                GetComponent<BoxCollider2D>().enabled = false;
+                GameManager.Instance.IsGameOver = true;
+                //调用结束面板
+            }
         }        
+    }
+
+    private bool IsRayPlatform()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rayDown.position, Vector2.down, 1f, platformLayer);
+        if (hit.collider != null && hit.collider.CompareTag("Platform"))
+        {
+            return true;
+        }
+        return false;
     }
 
     private void Jump()
